86e5d0fc14eaff7ac7a0c0f1c431c44bf1d18d46
Hoàng Duy Khánh <hdkhanhit@gmai.com>
2021-10-24 10:26:27 +0700

    config redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "@styles/globals.scss";
+import { Provider } from "react-redux";
+import React from "react";
+import { useStore } from "@redux/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const store = useStore(pageProps.initialReduxState);
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ redux/reducers/auth.reducer.js
@@ -0,0 +1,35 @@
+import * as types from "../constants";
+
+const initialState = {
+  token: typeof window !== "undefined" && localStorage.getItem("token"),
+  refreshToken:
+    typeof window !== "undefined" && localStorage.getItem("refreshToken"),
+  user: null,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.LOGIN:
+      return {
+        ...state,
+        token: action.token,
+        refreshToken: action.refreshToken,
+      };
+    case types.GET_PROFILE:
+      return {
+        ...state,
+        user: action.user,
+      };
+    case types.LOGOUT:
+      localStorage.clear();
+      return {
+        token: null,
+        refreshToken: null,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
